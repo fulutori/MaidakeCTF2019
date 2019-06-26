8d3c6aafe0a68fb335aa39c42935251789ed9b36
kobayashi9 <45445811+kobayashi9@users.noreply.github.com>
2019-06-26 22:43:44 +0900

    Add a secret image.
    Cryptoの500点問題を追加しました。
    コードは後で書き直したいです。
    とりあえず追加。

---
+++ Crypto/500_Secret_image/readme.md
@@ -0,0 +1,14 @@
+# Secret image
+
+## 問題文
+秘密鍵手に入れちゃったけどどうしよう…
+
+## 添付ファイル
+- problem.py
+- secretkey.txt
+
+## Flag
+MaidakeCTF{AES-ECB_mode_is_Vulnerable}
+
+## その他
+ソースがコピペなので書き直したい…
---
+++ Crypto/500_Secret_image/solve.py
@@ -0,0 +1,44 @@
+from PIL import Image
+from Crypto.Cipher import AES
+ 
+# AES requires that plaintexts be a multiple of 16, so we have to pad the data
+def pad(data):
+    return data + b"\x00"*(16-len(data)%16) 
+ 
+# Maps the RGB 
+def convert_to_RGB(data):
+    r, g, b = tuple(map(lambda d: [data[i] for i in range(0,len(data)) if i % 3 == d], [0, 1, 2]))
+    pixels = tuple(zip(r,g,b))
+    return pixels
+    
+def process_image(filename, key):
+    # Opens image and converts it to RGB format for PIL
+    im = Image.open(filename)
+    data = im.convert("RGB").tobytes()
+
+    # Since we will pad the data to satisfy AES's multiple-of-16 requirement, we will store the original data length and "unpad" it later.
+    original = len(data) 
+
+    # Decrypt using desired AES mode 
+    new1 = convert_to_RGB(aes_ecb_decrypt(key, pad(data))[:original])
+    
+    # Create a new PIL Image object and save the old image data into the new image.
+    im2 = Image.new(im.mode, im.size)
+    im2.putdata(new1)
+
+    #Save image
+    im2.save("solve.png")
+
+# ECB
+def aes_ecb_decrypt(key, data, mode=AES.MODE_ECB):
+    aes = AES.new(key, mode)
+    new_data = aes.decrypt(data)
+    return new_data
+
+def main():
+    filename = "problem.png"
+    key = "this_a_secretkey"
+    process_image(filename, key)
+
+if __name__ == "__main__":
+    main()
