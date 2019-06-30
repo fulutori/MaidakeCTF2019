5c7cbe7df9a027ed3bb9169a71420113991cdc7c
aokakes <fulutori@gmail.com>
2019-06-30 14:46:22 +0900

    QR Puzzle 2nd

---
+++ Misc/400_QR_Puzzle_2nd/make_puzzle.py
@@ -0,0 +1,33 @@
+from PIL import Image
+import os
+import sys
+import random
+
+def ImgSplit(img, width, height):
+	split = 4
+	split_height = height // split
+	split_width = width // split
+
+	buff = []
+	for h1 in range(split):
+		for w1 in range(split):
+			w2 = w1 * split_height
+			h2 = h1 * split_width
+			c = img.crop((w2, h2, split_width + w2, split_height + h2))
+			buff.append(c)
+	return buff
+
+
+
+if __name__ == '__main__':
+	path = './puzzle/'
+	if not os.path.isdir(path):
+		os.makedirs(path)
+	img = Image.open('qr.png')
+	width, height = img.size
+	puzzles = ImgSplit(img, width, height)
+	random.shuffle(puzzles)
+	
+	for idx, puzzle in enumerate(puzzles):
+		puzzle.save("{}{}.png".format(path,idx+1), "PNG")
+
