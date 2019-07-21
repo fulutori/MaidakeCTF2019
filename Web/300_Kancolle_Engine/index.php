5267f8ff769c6e6a2c659299abd85ed3c5af1cd4
aokakes <fulutori@gmail.com>
2019-07-21 17:33:59 +0900

    Update index.php

--- Web/300_Kancolle_Engine/index.php
+++ Web/300_Kancolle_Engine/index.php
@@ -30,25 +30,25 @@
 			<div class="col-12">
 				<form method="post">
 					<div class="form-group">
-					<input type="text" class="form-control mb-2 w-75 mx-auto" name="md5" placeholder="ex) c82cfc524aa9a7e402d7a508b491b57b" required />
+					<input type="text" class="form-control mb-2 w-75 mx-auto" name="name" placeholder="ex) haruna" required />
 					</div>
 					<p class="text-center"><button type="submit" class="w-50 py-2 my-2" name="search">検索</button></p>
 					<?php
 						if(isset($_POST['search'])) {
-							$md5 = htmlspecialchars($_POST['md5']);
+							$name = htmlspecialchars($_POST['name']);
 
 							$pdo = new PDO('sqlite:./sqlite/kancolle.db');
 							$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 							$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-							$sql = "SELECT * FROM ship WHERE md5='".$md5."'";
+							$sql = "SELECT * FROM ship WHERE name='".$name."'";
 
 							$search = $pdo->prepare($sql);
 							$search->execute();
 							$result = $search->fetchAll(PDO::FETCH_ASSOC);
 
-							if ($result[0]['md5'] == '') {
-								?><div class="alert alert-danger w-75 text-center mx-auto" role="alert">このハッシュ値の艦娘は存在しません</div><?php 
+							if ($result[0]['name'] == '') {
+								?><div class="alert alert-danger w-75 text-center mx-auto" role="alert">艦娘が存在しません</div><?php 
 							} else {
 								echo '<table class="table table-hover table-striped">';
 								echo '<thead><tr><th>Name</th><th>MD5</th></tr></thead>';
