3950e338f38282a1afbc5b7e005815cd45f9871a
aokakes <fulutori@gmail.com>
2019-07-24 15:58:21 +0900

    Baked goods

---
+++ Web/100_Baked_goods/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>Baked goods</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body background="./img/cookie.png">
+<div class="container">
+	<div class="page-header" id="banner">
+		<div class="row my-2">
+			<div class="col-12">
+				<h2 class="text-center mx-1 mt-5">Baked goods</h2>
+				<div class="mt-5">
+					<font size="5rem">
+						<p id="error"><font color="#F00">javascriptを有効にしてください</font></p>
+						<script type="text/javascript"> error.style.display = "none"; </script>
+					</font>
+				</div>
+				<p class="lead text-center mx-1 mt-1" id="result"></p>
+			</div>
+		</div>
+	</div>
+
+	<section class="bs-docs-section clearfix">
+		<div class="row">
+			<div class="col-12">
+				<p class="text-center">Do you like sweets?</p>
+			</div>
+		</div>
+	</section>
+
+	<footer class="footer">
+		<div class="container mt-5">
+			<p class="text-muted text-center">Copyright © aokakes All Rights Reserved.</p>
+		</div>
+	</footer>
+</div>
+</body>
+</html>
+<?php
+setcookie('flag','MaidakeCTF{Beware_of_cookie_theft}')
+?>
---
+++ Web/100_Baked_goods/readme.md
@@ -0,0 +1,13 @@
+# Baked goods
+
+## 問題文
+焼き菓子は好きですか？
+
+## 添付ファイル
+URL
+
+## Flag
+MaidakeCTF{Beware_of_cookie_theft}
+
+## 検証用URL
+https://aokakes.work/CTF/MaidakeCTF2019/Baked_goods/
